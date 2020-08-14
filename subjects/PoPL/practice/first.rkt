22d7bd577b1d655fe239550fb1c3097c7e48108f
zubairabid <zubairabid1999+github@gmail.com>
2020-08-14 19:49:02 +0530

    Nationalism Lec 2 done

--- subjects/PoPL/practice/first.rkt
+++ subjects/PoPL/practice/first.rkt
@@ -3,9 +3,31 @@
 (define (extract str)
   (substring str 1 4))
 
-(define (extract2 str)
-  (substring str 4 7))
+(define (piece str)
+  (substring str 0 pi))
 
-(define (extract3 str)
-  (substring str 4 7))
+(define (! n)
+  (if (= n 0)
+      1
+      (* n (! (- n 1)))))
 
+(define (reply-more s)
+  (cond
+   [(equal? "hello" (substring s 0 5))
+    "hi!"]
+   [(equal? "goodbye" (substring s 0 7))
+    "bye!"]
+   [(equal? "?" (substring s (- (string-length s) 1)))
+    "I don't know"]
+   [else "huh?"]))
+
+(define (add numbers)
+    (if (null? numbers)
+        0
+        (+ (car numbers) (add (cdr numbers)))))
+
+;; function that takes a function and a param and operates the function 
+;; twice on said parameter
+
+(define (twice func var)
+  (func (func var)))
