fc0429c06a4e4651057bfa7cdba327f11c400039
zubairabid <zubairabid1999+github@gmail.com>
2020-08-16 13:07:35 +0530

    Doing POPL HW01, Q1 done

--- subjects/PoPL/practice/first.rkt
+++ subjects/PoPL/practice/first.rkt
@@ -1,33 +1,71 @@
 #lang racket
 
-(define (extract str)
-  (substring str 1 4))
-
-(define (piece str)
-  (substring str 0 pi))
-
-(define (! n)
+(define (repeat n x)
   (if (= n 0)
-      1
-      (* n (! (- n 1)))))
+      '()
+      (cons x (repeat (- n 1) x))))
 
-(define (reply-more s)
+(define (invert lst)
+  (if (null? lst)
+      '()
+      (cons (reverse (car lst)) (invert (cdr lst)))))
+
+(define (count-occurrences s slist)
+  (define (occplus s comp)
+    (if (= s comp)
+        1
+        0))
+  (if (null? slist)
+      0
+      (+ (occplus s (car slist)) (count-occurrences s (cdr slist)))))
+
+(define (product sos1 sos2) 
   (cond
-   [(equal? "hello" (substring s 0 5))
-    "hi!"]
-   [(equal? "goodbye" (substring s 0 7))
-    "bye!"]
-   [(equal? "?" (substring s (- (string-length s) 1)))
-    "I don't know"]
-   [else "huh?"]))
+    [(null? sos1) sos2]
+    [(null? sos2) sos1]
+    [else 
+      (cond
+        [(> (length sos1) 1)
+         (append (product (list (car sos1)) sos2) (product (cdr sos1) sos2))]
+        [(> (length sos2) 1)
+         (append (product sos1 (list (car sos2))) (product sos1 (cdr sos2)))]
+        [else (list (append sos1 sos2))])]))
 
-(define (add numbers)
-    (if (null? numbers)
-        0
-        (+ (car numbers) (add (cdr numbers)))))
 
-;; function that takes a function and a param and operates the function 
-;; twice on said parameter
+(define (every pred lst) 
+  (if (null? lst)
+      #t
+      (and (pred (car lst)) (every pred (cdr lst)))))
 
-(define (twice func var)
-  (func (func var)))
+(define (merge loi1 loi2)
+  (cond 
+    [(null? loi1) loi2]
+    [(null? loi2) loi1]
+    [else 
+      (if (< (car loi1) (car loi2))
+             (append (list (car loi1)) (merge (cdr loi1) loi2))
+             (append (list (car loi2)) (merge loi1 (cdr loi2))))]))
+
+(define (flatten dlst)
+  (cond
+    [(null? dlst) '()]
+    [(not (list? dlst)) (list dlst)]
+    [else (append (flatten (car dlst)) (flatten (cdr dlst)))]))
+
+(require eopl)
+ 
+(define-datatype full-binary-tree fullbin
+  (internal-node (value integer?)
+                 (left-child fullbin)
+                 (right-child fullbin))
+  (leaf-node (value integer?)))
+
+
+(define inode
+  (lambda (v l r)(internal-node v l r)))
+
+(define lnode
+  (lambda (v)(leaf-node v)))
+
+
+
